efdddd88f8b6f812ffa34fd8d4341ed1eba84952
olgawaszczuk <waszczuko@op.pl>
2017-05-19 15:03:01 +0200

    Update RPS.py

--- laby 8 - 9/RPS.py
+++ laby 8 - 9/RPS.py
@@ -1,9 +1,26 @@
-#Problem dualny
+#Problem dualny do problemu RPS
 matrixA = ([1, 0 , -1 , 1] , [1, 1, 0, -1], [1, -1, 1, 0], [0, 1, 1, 1] ,[0, 1, 1, 1])
 vector_b = (0, 0, 0,1, 1)
 vector_c = (1, 0, 0, 0)
 
 constraints = ["<=", "<=", "<=", "<=", ">="]
+variables = ["", ">=", ">=", ">="]
+
+P = InteractiveLPProblem(matrixA, vector_b, vector_c, ["z", "x1", "x2", "x3"], constraint_type = constraints, variable_type=variables)
+show(P)
+
+#Zamiana problemu do postaci normalnej
+Pn = P.standard_form()
+show(Pn)
+
+show(Pn.run_simplex_method())
+
+#Po pozbawieniu jednego gracza ruchu:
+matrixA = ([1, 0 , -1 , 1] , [1, 1, 0, -1], [0, 1, 1, 1] ,[0, 1, 1, 1])
+vector_b = (0, 0, 1, 1)
+vector_c = (1, 0, 0, 0)
+
+constraints = ["<=", "<=", "<=", ">="]
 variables = ["", ">=", ">=", ">="]
 
 P = InteractiveLPProblem(matrixA, vector_b, vector_c, ["z", "x1", "x2", "x3"], constraint_type = constraints, variable_type=variables)
