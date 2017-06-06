d00198467aa9943185d6ef2328f125d36df8dcbd
olgawaszczuk <waszczuko@op.pl>
2017-06-06 18:08:57 +0200

    Update p.py

--- p.py
+++ p.py
@@ -1,10 +1,29 @@
 # Maksymalna funkcja celu
 def maximal_objective_entering(self):
+    set = self.possible_entering()
     objective_value = self.objective_value
-    for possible in self:
+    entering = 0
+    for possible in set:
         self.enter(possible)
-        self.update
+        self.update()
         if self.objective_value() > objective_value:
             obejctive_value = self.objective_value()
             entering = possible
+    return(entering)
+
+def maximal_objective_entering(self):
+    set_entering = self.possible_entering()
+    set_leaving=self.possible_leaving()
+    objective_value = self.objective_value
+    entering = 0
+    leaving = 0
+    for entering_variable in set_entering:
+        for leaving_variable in set_leaving:
+            self.enter(entering_variable)
+            self.leave(leaving_variable)
+            self.update()
+            if self.objective_value() > objective_value:
+                obejctive_value = self.objective_value()
+                entering = entering_variable
+                leaving = leaving_variable
     return(entering)
